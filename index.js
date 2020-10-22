1e9d13391450be9cb01995a1490068f6a7ba3854
Vanessa Cattabiani <vanebrunocattabiani@gmail.com>
2020-10-22 14:25:00 +0200

    Add some animation

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ const stoprotate = (event) => {
   const img = event.currentTarget.parentNode.previousSibling.previousSibling.lastChild.previousSibling;
   img.classList.remove("rotate");
 };
+const countAlbum = () => {
+  const albums = document.querySelectorAll(".albumcontainer").length;
+  const albumcovers = document.getElementById("tapestry").querySelectorAll("img").length;
+  const destination = document.getElementsByTagName("footer")[0].querySelector("div+p");
+  destination.innerText += albumcovers + albums;
+};
+window.onload = countAlbum;
